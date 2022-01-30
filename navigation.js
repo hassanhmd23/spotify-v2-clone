438d3ad5dcda58f3585cf4713b8d9e4ebba8e56d
hassanhmd23 <hassanhmd1734@gmail.com>
2022-01-30 13:14:46 +0200

    Adjust Sidebar with shorter code

---
+++ navigation.js
@@ -0,0 +1,35 @@
+import {
+    HomeIcon,
+    SearchIcon,
+    LibraryIcon,
+    PlusCircleIcon,
+    HeartIcon,
+    RssIcon,
+  } from "@heroicons/react/outline";
+const navigation = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Search",
+    icon: SearchIcon,
+  },
+  {
+    name: "Your Library",
+    icon: LibraryIcon,
+  },
+  {
+    name: "Create Playlist",
+    icon: PlusCircleIcon,
+  },
+  {
+    name: "Liked Songs",
+    icon: HeartIcon,
+  },
+  {
+    name: "Your episodes",
+    icon: RssIcon,
+  },
+];
+export default navigation;
